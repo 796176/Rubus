1b3504855483d690e8315501eaacbe72f926a2bf
yav <796176@protonmail.com>
2025-02-17 12:18:30 +0300

    Remove the javafx dependency
    Intellij idea was tested by monkeys, which is why it added a lot of file
    changes it wasn't supposed to.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
 plugins {
     id("java")
-    id("org.openjfx.javafxplugin") version "0.1.0"
 }
-
-javafx {
-    modules("javafx.controls", "javafx.fxml", "javafx.media")
-}
-
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
